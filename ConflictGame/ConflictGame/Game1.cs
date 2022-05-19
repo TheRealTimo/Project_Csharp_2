a241280f54ffa6ec30aa0edf39d3272b35f35d2a
Timo <31155789+TheRealTimo@users.noreply.github.com>
2022-05-19 20:53:29 +0200

    Working user input keyboard
    Added keyboard controls and ball graphics

--- ConflictGame/ConflictGame/Game1.cs
+++ ConflictGame/ConflictGame/Game1.cs
@@ -6,6 +6,10 @@ namespace ConflictGame
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;  //Loads the texture of the ball in 2D
+        Vector2 ballPosition; //Saves the X and Y axis as vector
+        float ballSpeed;  //RemoveAll
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -20,12 +24,18 @@ namespace ConflictGame
         {
             // TODO: Add your initialization logic here
 
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, //From here
+            _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;  //Till here
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ballTexture = Content.Load<Texture2D>("ball"); //Specifies the content that has to be loaded in; Remove
 
             // TODO: use this.Content to load your game content here
         }
@@ -34,6 +44,32 @@ namespace ConflictGame
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            var kstate = Keyboard.GetState();  //From here
+
+            if (kstate.IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2) //Checks that the ball cannot leave the game area
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if (ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if (ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
+
+            base.Update(gameTime); //Until here
 
             // TODO: Add your update logic here
 
@@ -45,6 +81,19 @@ namespace ConflictGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();  //From here
+            _spriteBatch.Draw(ballTexture,
+            ballPosition, //Centers the ball position to the center of the png
+            null,
+            Color.White,
+            0f,
+            new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+            );
+            _spriteBatch.End();  //Till here
 
             base.Draw(gameTime);
         }
