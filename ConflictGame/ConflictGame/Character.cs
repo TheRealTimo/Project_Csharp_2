30a7394f05f47182912866ee5c890d8fbac534b5
3360260 <72194187+3360260@users.noreply.github.com>
2022-05-20 14:00:57 +0200

    First attempt at gravity
    it doesn't seem to understand the gravity of the situation

---
+++ ConflictGame/ConflictGame/Character.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConflictGame
+{
+    class Character
+    {
+        Texture2D texture;
+
+        Vector2 position;
+        Vector2 velocity;
+
+        bool hasJumped;
+
+        private GraphicsDeviceManager _graphics;
+
+        public Character(Texture2D newTexture, Vector2 newPosition)
+        {
+            texture = newTexture;
+            position = newPosition;
+            hasJumped = true;
+        }
+
+        protected void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            position = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            position += velocity;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                velocity.X = 3f;
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                velocity.X = -3f;
+
+            else
+                velocity.X = 0f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && hasJumped == false)
+            {
+                position.Y -= 10f;
+                velocity.Y = -5f;
+                hasJumped = true;
+            }
+
+            if (hasJumped == true)
+            {
+                float i = 1;
+                velocity.Y += 0.15f * i;
+            }
+
+            if(position.X + texture.Height >= 150)
+            {
+                hasJumped = false; 
+            }
+
+            if (hasJumped == false)
+            {
+                velocity.Y = 0f;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
--- ConflictGame/ConflictGame/Game1.cs
+++ ConflictGame/ConflictGame/Game1.cs
@@ -6,8 +6,14 @@ namespace ConflictGame
 {
     public class Game1 : Game
     {
+        //Texture2D ballTexture;  //Loads the texture of the ball in 2D
+        //Vector2 ballPosition; //Saves the X and Y axis as vector
+        //float ballSpeed;  //RemoveAll
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Character player;
 
         public Game1()
         {
@@ -20,6 +26,10 @@ namespace ConflictGame
         {
             // TODO: Add your initialization logic here
 
+            /*ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, //From here
+            _graphics.PreferredBackBufferHeight / 2);
+            //ballSpeed = 100f;  //Till here
+            */
             base.Initialize();
         }
 
@@ -27,6 +37,8 @@ namespace ConflictGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //ballTexture = Content.Load<Texture2D>("ball"); //Specifies the content that has to be loaded in; Remove
+            player = new Character(Content.Load<Texture2D>("head"), new Vector2(50, 50));
             // TODO: use this.Content to load your game content here
         }
 
@@ -34,6 +46,36 @@ namespace ConflictGame
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            player.Update(gameTime);
+
+            /* Old Control scheme
+            var kstate = Keyboard.GetState();  //From here
+
+            if (kstate.IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2) //Checks that the ball cannot leave the game area
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if (ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if (ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
+            */
+
+            base.Update(gameTime); //Until here
 
             // TODO: Add your update logic here
 
@@ -45,6 +87,36 @@ namespace ConflictGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();  //From here
+
+            player.Draw(_spriteBatch);
+
+            /* Old Ball Texture
+        _spriteBatch.Draw(ballTexture,
+        ballPosition, //Centers the ball position to the center of the png
+        null,
+        Color.White,
+        0f,
+        new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+        Vector2.One,
+        SpriteEffects.None,
+        0f
+        );
+
+        /* Old Ball Texture
+        _spriteBatch.Draw(ballTexture,
+        ballPosition, //Centers the ball position to the center of the png
+        null,
+        Color.White,
+        0f,
+        new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+        Vector2.One,
+        SpriteEffects.None,
+        0f
+        );
+        */
+            _spriteBatch.End();  //Till here
 
             base.Draw(gameTime);
         }
