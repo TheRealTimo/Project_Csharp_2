e5aed043e7ed1d7312d20bd6ecf82e49efbf4bf8
HolyMelonSeed <sikora2012alex@gmail.com>
2022-05-20 14:09:16 +0200

    Spritework
    Added art, including the new jump animation
    Made animated sprites work with use of Monogame.Extended NuGet package

--- ConflictGame/ConflictGame/Game1.cs
+++ ConflictGame/ConflictGame/Game1.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Xna.Framework;
+﻿    using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 
 namespace ConflictGame
 {
     public class Game1 : Game
     {
+        private AnimatedSprite _gerjanSprite;
+        private Vector2 _gerjanPosition;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -26,6 +32,12 @@ namespace ConflictGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            var spriteSheet = Content.Load<SpriteSheet>("gerjan.sf", new JsonContentLoader());
+            var sprite = new AnimatedSprite(spriteSheet);
+
+            sprite.Play("idle");
+            _gerjanPosition = new Vector2(100, 100);
+            _gerjanSprite = sprite;
 
             // TODO: use this.Content to load your game content here
         }
@@ -36,6 +48,38 @@ namespace ConflictGame
                 Exit();
 
             // TODO: Add your update logic here
+            var deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var walkSpeed = deltaSeconds * 128;
+            var keyboardState = Keyboard.GetState();
+            var animation = "idle";
+
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+            {
+                animation = "runright";
+                _gerjanPosition.Y -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+            {
+                animation = "runright";
+                _gerjanPosition.Y += walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                animation = "runleft";
+                _gerjanPosition.X -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+            {
+                animation = "runright";
+                _gerjanPosition.X += walkSpeed;
+            }
+
+            _gerjanSprite.Play(animation);
+
+            _gerjanSprite.Update(deltaSeconds);
 
             base.Update(gameTime);
         }
@@ -45,6 +89,9 @@ namespace ConflictGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_gerjanSprite, _gerjanPosition);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
