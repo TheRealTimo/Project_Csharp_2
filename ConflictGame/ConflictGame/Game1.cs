31cee696f29fc0317385b7a4ee257e887383205e
KeanuSTJ <keanustjago@hotmail.com>
2022-05-20 10:11:52 +0200

    added splashscreen and threads

--- ConflictGame/ConflictGame/Game1.cs
+++ ConflictGame/ConflictGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading.Tasks;
 
 namespace ConflictGame
 {
@@ -8,6 +9,11 @@ namespace ConflictGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        
+        private Task backgroundTask;
+        private bool isLoading;
+        private Splashscreen splashscreen;
+
 
         public Game1()
         {
@@ -19,19 +25,42 @@ namespace ConflictGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+
+            _graphics.IsFullScreen = true;
+
+            //_graphics.ApplyChanges();
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            isLoading = true;
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            backgroundTask = new Task(this.BackgroundTask);
+            backgroundTask.Start();
+            // Main thread builds splashscreen
+            splashscreen = new Splashscreen(this, Content, _graphics);
+           // _currentState = loadingState;
             // TODO: use this.Content to load your game content here
+        }
+        public void BackgroundTask()
+        {
+            // field for backround task to be done while splashscreen is displayed
+            //backgroundTask.Wait(200);
         }
 
         protected override void Update(GameTime gameTime)
         {
+            if (backgroundTask.IsCompleted)
+            {
+                backgroundTask.Dispose();
+                isLoading = false;
+                // Change State
+            }
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -43,7 +72,7 @@ namespace ConflictGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            if (isLoading) { splashscreen.Draw(gameTime, _spriteBatch); }
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ ConflictGame/ConflictGame/Splashscreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConflictGame
+{
+    class Splashscreen
+    {
+        private GraphicsDeviceManager graphics;
+        private readonly Texture2D titleCard;
+        private Vector2 titlePosition;
+        private ContentManager content;
+        public Splashscreen(Game1 game, ContentManager content, GraphicsDeviceManager graphics)
+          //: base(game, graphicsDevice, content)
+        {
+            this.content = content;
+            this.graphics = graphics;
+            this.titleCard = content.Load<Texture2D>("ConflictPixelTinyLogo");
+            this.titlePosition = new Vector2((int)(graphics.PreferredBackBufferWidth * 1.45), (int)(graphics.PreferredBackBufferHeight * 1.5));
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(content.Load<Texture2D>("Loadingbackground"), new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
+
+            spriteBatch.Draw(titleCard, titlePosition, Color.White);
+            spriteBatch.End();
+        }
+
+        public  void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
