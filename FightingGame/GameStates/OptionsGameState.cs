a77acaa1d893fd16f30cf8b7047a0e96d6d887bb
Timo <31155789+TheRealTimo@users.noreply.github.com>
2022-06-23 10:19:26 +0200

    Options Back
    Added Back Button on Options Menu

--- FightingGame/GameStates/OptionsGameState.cs
+++ FightingGame/GameStates/OptionsGameState.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using FightingGame.Controls;
+using System;
 
 namespace FightingGame.GameStates
 {
     public class OptionsGameState : GameState
     {
-        public OptionsGameState(FightingGame game) : base(game) { }
+        private readonly Button _backButton;
+
+        public OptionsGameState(FightingGame game) : base(game)
+        {
+            _backButton = new Button(this, new Vector2(753, 750), new Rectangle(0, 0, 413, 98));
+
+            _backButton.OnClick += OnBackButtonClick;
+
+            GameComponents.Add(_backButton);
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            _backButton.Texture = Game.Textures["BackButton"];
+        }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
@@ -15,6 +32,10 @@ namespace FightingGame.GameStates
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+        }
+        private void OnBackButtonClick(object sender, EventArgs e)
+        {
+            Game.ChangeGameState(Game.GameStates["Menu"]);
         }
     }
 }
